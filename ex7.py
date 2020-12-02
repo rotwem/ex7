ac3b69aa351d2f1b22013ab311fd7b710eddbe4f
Rotem Shadur <rotwem@gmail.com>
2020-12-02 17:26:38 +0200

    initial commit: ex7 - function details + helper files

---
+++ ex7.py
@@ -0,0 +1,49 @@
+# PART 1
+# can't use any kind of loops
+
+
+def print_to_n(n):
+    """receives an int and returns akk ints from 1 to n (if input under 1 prints nothing)"""
+    pass
+
+
+def digit_sun(n):
+    """receives a positive int and returns the sum of the ints digits (can't use strings)"""
+    pass
+
+
+def is_prime(n):
+    """receives an int and returns True\False if the number is prime"""
+    pass
+
+
+def _has_divisor_smaller_than(n, i):
+    """helper for is_prime(n) - cheacks if n has a devider that is smaller than i"""
+    pass
+
+
+# PART 2
+# can use loops from here
+
+def play_hanoi(hanoi, n, src, dst, temp):
+    pass
+
+
+def print_sequences(char_list, n):
+    """receives an a string of chars and prints all the potential combinations with n length with repetitions"""
+    pass
+
+
+def print_no__repetition_sequences(char_list, n):
+    """receives an a string of chars and prints all the potential combinations with n length with no repetitions"""
+    pass
+
+
+def parentheses(n):
+    """"""
+    pass
+
+
+def flood_fill(image, start):
+    """"""
+    pass
